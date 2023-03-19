ae38837ad0c0f45e2c8e684962e301b45d49b3bd
Ban Alex <alexbanut10@gmail.com>
2023-03-19 21:37:13 +0200

    event listeners and managing data state with useState in React by builing a meme genetator

--- 4.interactive-app/src/components/Meme.jsx
+++ 4.interactive-app/src/components/Meme.jsx
@@ -1,11 +1,36 @@
+import memesData from '../memesData';
+import React from 'react';
+
 export default function Meme() {
+	const [meme, changeMeme] = React.useState({
+		topText: '',
+		bottomText: '',
+		memePic: '../images/memeimg.png'
+	});
+
+	const [allMemeImages, changeMemes] = React.useState(memesData);
+ 
+	function newMemeImage(props) {
+		const memesArray = memesData.data.memes;
+		const rand = Math.floor(Math.random() * memesArray.length);
+		const url = memesArray[rand].url;
+		changeMeme( (oldMeme) => {
+			return {
+				...oldMeme,
+				memePic: url
+			};
+		});
+	}
+
 	return (
 		<main>
-			<form className="meme-form">
+			<div className="meme-form">
 				<input type="text" placeholder="Top text" className="meme-input" />
 				<input type="text" placeholder="Bottom text" className="meme-input" />
-				<button type="submit" className="meme-button">Get new meme image</button>
-			</form>
+				{/* react event listener */}
+				<button onClick={newMemeImage} className="meme-button">Get new meme image</button>
+			</div>
+			<img src={meme.memePic} alt="a random meme" className="meme-image"></img>
 		</main>
 	);
 }
