03ab720003f7346922cd92102e4c8a75c7daac68
Alex Ban <alexbanut10@gmail.com>
2023-03-20 18:00:09 +0200

    passing state thru DOM tree components, more React state concepts and forms in react using state

--- 4.interactive-app/src/components/Meme.jsx
+++ 4.interactive-app/src/components/Meme.jsx
@@ -9,6 +9,11 @@ export default function Meme() {
 	});
 
 	const [allMemeImages, changeMemes] = React.useState(memesData);
+
+	const [memeForm, changeMemeForm] = React.useState({
+		topText: "",
+		bottomText: ""
+	});
  
 	function newMemeImage(props) {
 		const memesArray = memesData.data.memes;
@@ -22,15 +27,52 @@ export default function Meme() {
 		});
 	}
 
+	function handleChange(event) {
+		const { name, value } = event.target;
+		changeMemeForm( (prevFormData) => {
+			return {
+				...prevFormData,
+				[name]: value
+			}
+		});
+	}
+
+	// let topTextMeme;
+	// let bottomTextMeme;
+	// function handleSubmit(event) {
+	// 	event.preventDefault();
+	// 	console.log("top text: ",memeForm.topText, "bottom text: ",memeForm.bottomText);
+	// 	topTextMeme = memeForm.topText;
+	// 	bottomTextMeme = memeForm.bottomText;
+	// }
+
 	return (
 		<main>
-			<div className="meme-form">
-				<input type="text" placeholder="Top text" className="meme-input" />
-				<input type="text" placeholder="Bottom text" className="meme-input" />
+			<form className="meme-form" /*onSubmit={handleSubmit} */>
+				<input 
+					type="text"
+					placeholder="Top text"
+					className="meme-input"
+					onChange={handleChange}
+					name="topText"
+					value={memeForm.topText}
+				/>
+				<input
+					type="text"
+					placeholder="Bottom text"
+					className="meme-input"
+					onChange={handleChange}
+					name="bottomText"
+					value={memeForm.bottomText}
+				/>
 				{/* react event listener */}
 				<button onClick={newMemeImage} className="meme-button">Get new meme image</button>
+			</form>
+			<div className="meme">
+				<img src={meme.memePic} alt="a random meme" className="meme-image"></img>
+				<h2 className='meme-text top'>{memeForm.topText}</h2>
+				<h2 className='meme-text bottom'>{memeForm.bottomText}</h2>
 			</div>
-			<img src={meme.memePic} alt="a random meme" className="meme-image"></img>
 		</main>
 	);
 }
